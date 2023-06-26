93b74d41fe094e9506586c75b73442a158768650
Colin Osborn <colin.osborn@gmail.com>
2023-06-25 21:20:44 -0600

    corrected sign out route and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
-
-  root "pages#index"
-
+  devise_for :users, :controllers => { :registrations => "registrations" }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  root "pages#index"
 end
